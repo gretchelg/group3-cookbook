f5e02bd2b3206a283f0ab8c1a22d980f3f3efb5a
Timuzzo <t.myasnikov@gmail.com>
2023-04-27 10:37:36 +0200

    API fetch, css reset, basic data import structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { Client } from "./Client"
+import { useState, useEffect } from 'react';
+import {Recipe} from "./components/Recipe"
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  console.log(recipes);
+
+  useEffect(() => {
+    Client.getEntries().then((data) => {
+      console.log(data.items)
+      setRecipes(data.items)
+      }).catch((error) => console.log(error));
+  }, [])
+  // const arrayIngredients = recipe.fields.ingredients 
+
   return (
     <div className="App">
-      Cookbook 
+      {recipes?.map( (recipe) => (
+        <div>
+        <h1>{recipe.fields.recipeTitle}</h1>
+        <ul>
+          <li>{recipe.fields.type}</li>
+          <li>{recipe.fields.description}</li>
+          <li>
+            <img src={recipe.fields.pic.fields.file.url} alt={recipe.fields.recipeTitle} /></li>
+        </ul>
+          <div>
+            <ol>
+              {recipe.fields.ingredients.map ((item) => (
+                <li>{item}</li>
+              ))
+              }
+            </ol>
+          </div>
+      </div>
+      ))}
     </div>
   );
 }
